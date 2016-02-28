d6e05d3cf13f92e47862d9111e73d9047e734e1d
Zhongyang Gao <billgao0807@gmail.com>
2016-02-27 20:57:08 -0800

    buy/sell codes

--- php/user.php
+++ php/user.php
@@ -1,7 +1,7 @@
 <?php
 
 Class User {
-	
+
 	private $username;
 	private $password;
 	private $balance;
@@ -11,24 +11,25 @@ Class User {
 	public function __construct ($username, $password){
 		$this->$username = $username;
 		$this->$password = $password;
-		
+
 		#get starting balance from Parse database
-		$query = new ParseQuery("accountBalance");
+		$query = new ParseQuery("User");
 		$query->equalTo("username", $username);
-		$results = $query->find();
+		$results = $query->first();
 		#echo "Successfully retrieved " . count($results) . " balances.";
 		#there should be only one balance for each user
-		$this->balance = $results[0];
+		$this->balance = $results->get("balance");
+		$this->stocks = populateStocks($results->get("stocks"));
 	}
 
 	public function buyStock ($ticker, $companyName, $quantity){
-        #check currentBalance is sufficient
-		#look up stock and confirm it is valid
-		#if valid retrieve current price
-		#check current time to see if market is open
-		if(!isset($stocks)){
-			//create the array with the new stock info
-			echo 'Stock bought';
+
+		$result = getQuote($ticker, "na");
+
+		$currPrice = $result[1];
+
+		if( ($balance - $currPrice*$quantity) > 0 ) {
+			addStock($ticker, $quantiry);
 		}
 
 	}
@@ -40,11 +41,33 @@ Class User {
 		if(!isset($stocks)){
 			//no stocks to sell
 			echo 'Stock sold';
-		} 
+		}
 
 	}
-	
+
 	public function importCSVFile(){
 
 	}
+
+	private function getQuote($ticker, $format) {
+		$data = file_get_contents("http://finance.yahoo.com/d/quotes.csv?s=$ticker&f=$format&e=.csv");
+		$result = explode( ',', $data);
+
+		return $result;
+	}
+
+	private function populateStocks($stocksID) {
+		$query = new ParseQuery("Stock");
+		foreach ($stocksID as $stockID) {
+			try {
+
+				$stock = $query->get($stockID);
+				$stocks[$stock->get("tickerSymbol")] = $stock;
+			} catch (ParseException $ex) {
+				echo "error popolating stocks array with a spscific objectID";
+			}
+		}
+
+	}
+
 }
