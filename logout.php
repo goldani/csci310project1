e914fbdc2ab0bc25efa2f95a249a5617430dbaae
iamchristianv <iamchristianv@gmail.com>
2016-02-28 14:29:36 -0800

    updated Parse initialization in logout

--- logout.php
+++ logout.php
@@ -6,8 +6,8 @@
     date_default_timezone_set('America/New_York');
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
+        ParseClient::initialize('W78hSNsME23VkGSZOD0JXn2XoM5Nf6GO41BgMqxE', 'H3EgW9gCr6wyP8MfL3Eobz1mWJMwydyp6N2prcVF', 'mRppu4ciMuqhNsTXHoeh329Za4ShOOc1F1NN0skD');   
     }   
-    ParseClient::initialize('W78hSNsME23VkGSZOD0JXn2XoM5Nf6GO41BgMqxE', 'H3EgW9gCr6wyP8MfL3Eobz1mWJMwydyp6N2prcVF', 'mRppu4ciMuqhNsTXHoeh329Za4ShOOc1F1NN0skD');   
 	ParseUser::logOut();
-	header('Location: /');
+	header('Location: /index.php');
 ?>
