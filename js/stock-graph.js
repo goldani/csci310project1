e8bf766ec9a74a538535741d97b5bdf56004b329
Rena Chen <renachen@usc.edu>
2016-02-28 14:01:48 -0800

    commiting for now

---
+++ js/stock-graph.js
@@ -0,0 +1,44 @@
+var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+//For changing the time range display on the graph.
+var setTimeIncrement = function(timebutton_id){
+	//get time value want to investigate.
+	//access api for data in this range...do this in another class or file. Write a function that takes in the time, and then return all the data in an array.
+	//
+
+}
+/* --- [Private functions] --- */
+
+/* --- [Functions and variables directly used by the graph] --- */
+var lineChartData = {
+	labels : ["January","February","March","April","May","June","July"],
+		datasets : [
+			{
+				label: "My First dataset",
+				fillColor : "rgba(220,220,220,0.2)",
+				strokeColor : "rgba(220,220,220,1)",
+				pointColor : "rgba(220,220,220,1)",
+				pointStrokeColor : "#fff",
+				pointHighlightFill : "#fff",
+				pointHighlightStroke : "rgba(220,220,220,1)",
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+			},
+			{
+				label: "My Second dataset",
+				fillColor : "rgba(151,187,205,0.2)",
+				strokeColor : "rgba(151,187,205,1)",
+				pointColor : "rgba(151,187,205,1)",
+				pointStrokeColor : "#fff",
+				pointHighlightFill : "#fff",
+				pointHighlightStroke : "rgba(151,187,205,1)",
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+			}
+		]
+
+	}
+
+window.onload = function(){
+	var ctx = document.getElementById("canvas").getContext("2d");
+	window.myLine = new Chart(ctx).Line(lineChartData, {
+		responsive: true
+	});
+}
