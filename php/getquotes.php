96d5bdcbc4ab3ba0a067340646885ac7148f7c7e
Zhongyang Gao <billgao0807@gmail.com>
2016-02-28 17:17:48 -0800

    test with AJAX

--- php/getquotes.php
+++ php/getquotes.php
@@ -1,10 +1,22 @@
 <?php
 
-private function getQuote($ticker, $format) {
-  $data = file_get_contents("http://finance.yahoo.com/d/quotes.csv?s=" . $ticker. "&f=" . $format . "&e=.csv");
-  $result = explode( ',', $data);
+$ticker = $_GET['ticker'];
+$format = $_GET['format'];
+// $ticker = "AAPL";
+// $format = "na";
+$data = file_get_contents("http://finance.yahoo.com/d/quotes.csv?s=" . $ticker. "&f=" . $format . "&e=.csv");
+// // $result = explode( ',', $data);
+//
+echo $data;
 
-  return $result;
-}
 
+//$localtime = localtime(time(), true);
+//echo json_encode($localtime);
+
+
+// var url = 'http://query.yahooapis.com/v1/public/yql';
+// var startDate = '2016-02-22';
+// var endDate = '2016-02-23';
+// var data = encodeURIComponent('select * from yahoo.finance.historicaldata where symbol in ("YHOO","AAPL","GOOG","MSFT") and startDate = "' + startDate + '" and endDate = "' + endDate + '"');
+// $.getJSON(url, 'q=' + data + "&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json", callback);
 ?>
