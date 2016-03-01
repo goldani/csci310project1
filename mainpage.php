94b542fa5f98cd240721c4717786e64bbc1611ef
Rena Chen <renachen@usc.edu>
2016-02-29 19:17:31 -0800

    get rid of stash stuff

--- mainpage.php
+++ mainpage.php
@@ -72,11 +72,11 @@
 	    		</div>
 
 	    		<div id="graph-section" class="widget-box">
-<<<<<<< Updated upstream
+
 	    			<canvas id="canvas"></canvas>
-=======
+
 	    			<canvas id="canvas" height="100px" width="200px"></canvas>
->>>>>>> Stashed changes
+
 	    			<div class="button-wrapper">
 	    				<button id="1d" class="button graph-button" onclick="updateTimeRange('1d')">1 day</button>
 	    				<button id="5d" class="button graph-button">5 days</button>
