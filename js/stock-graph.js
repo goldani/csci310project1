08a6d5a4b0ed4c782a59dbfd4e391b5fc131e4fd
Rena Chen <renachen@usc.edu>
2016-02-29 17:43:01 -0800

    chart stuff

--- js/stock-graph.js
+++ js/stock-graph.js
@@ -1,14 +1,71 @@
 var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
-//For changing the time range display on the graph.
-var setTimeIncrement = function(timebutton_id){
-	//get time value want to investigate.
-	//access api for data in this range...do this in another class or file. Write a function that takes in the time, and then return all the data in an array.
-	//
+var stocks = [];
+
+
+
+
+//For changing the time range display on the graph. Called by time buttons
+function updateTimeRange(time_range){
+	var num = time_range.substring(0, 1);
+	var timeUnit = time_range.substring(1);
+	var labels= [];
+	
+	myLine.removeData();
+	myLine.addData([50, 30] ,"What");
 
 }
-/* --- [Private functions] --- */
 
-/* --- [Functions and variables directly used by the graph] --- */
+//add a stock
+function addStock(ticker){
+	if(getIndexOf(ticker) < 0){
+		stocks.push(ticker);
+	}
+
+}
+//remove stock
+function removeStock(ticker){
+	var index = getIndexxOf(ticker);
+	if(index > -1){
+		stocks.splice(index, 1);
+	}
+}
+//set line color
+function setLineColor(){
+
+}
+
+/* -- [Helper functions] --- */
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+function getMonthsBeforeCurrentTime(num_months){
+	var monthArray = [];
+	var currentDate = new Date();
+	var mm = currentDate.getMonth();
+	var yyyy = currentDate.getYear();
+	while(num_months > 0){
+		mm = (mm - num_months) % 12; //make sure within 12 month range
+		monthArray.push(months[mm-1]); //mm-1 to match array indexing
+		num_months--;
+	}
+	return monthArray;
+}
+
+function getDaysBeforeCurrentTime(num_days){
+
+}
+
+function getIndexOf(ticker){
+	for(i=0; i<stocks.length; i++){
+		if(stocks[i] == ticker){
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+/* --- [Functions and variables directly used by the graph: the equivalent of main] --- */
+
 var lineChartData = {
 	labels : ["January","February","March","April","May","June","July"],
 		datasets : [
@@ -38,7 +95,7 @@ var lineChartData = {
 
 window.onload = function(){
 	var ctx = document.getElementById("canvas").getContext("2d");
-	window.myLine = new Chart(ctx).Line(lineChartData, {
+	myLine = new Chart(ctx).Line(lineChartData, {
 		responsive: true
 	});
 }
