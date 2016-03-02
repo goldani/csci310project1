a260d8d8e1334ed935c258fbb82622bc9b6ded22
Garrett Oldani <oldani@usc.edu>
2016-03-02 02:32:46 -0800

    Code to download file

---
+++ downloadUserManual.php
@@ -0,0 +1,15 @@
+<?php
+	$file = '/umanual/StockOverflowUserManual.docx';
+	if(file_exists($file)){
+		echo "test";
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/octet-stream');
+		header('Content-Disposition: attachment; filename="'.basename($file).'"');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($file));
+		readfile($file);
+		exit;
+	}
+?>
