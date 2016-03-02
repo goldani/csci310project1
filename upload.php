bb07bfbdf8a93da09bfc9673b4e908fabccd8b64
edgarlug <edgarlug@usc.edu>
2016-03-01 18:46:40 -0800

    file upload backend ready

--- upload.php
+++ upload.php
@@ -1,34 +1,33 @@
 <?php
-    #need to add this to php.ini:
-    #file_uploads = On
-    $directory = "/uploads";
-    $target_dir = sys_get_temp_dir();//"uploads/;
+
+    //get file from temporary direcory where it is stored
+    $target_dir = sys_get_temp_dir();
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOK = 1;
     $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
 	if($fileType != "csv"){
 		echo "Not a CSV file";
+    //do pop up instead of echo
 		$uploadOK = 0;
 	}
 	if($uploadOK == 0){
+    //do pop up instead of echo
 		echo "File not imported";
 	}
 	else{
-		//if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $directory)){
+		//if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
 			//echo "The file " . basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
-      if (($handle = fopen($target_file, "r")) !== FALSE) {
-        while (($stocksInfo = fgetcsv($handle, 1000, ",")) !== FALSE) {
-          $size = count($stocksInfo);
-          echo '------------------------------'. "\n";
-          $row++;
 
-          //Stock information from API
-          $symbol = $stocksInfo[0];
-          $stockName = $stocksInfo[1];
-          $currentPrice = $stocksInfo[2];
-          $previousClose = $stocksInfo[3];
-		}
-		else{
+      if (($file = fopen($target_file, "r")) !== FALSE) {
+        while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
+            $ticker = $data[0];
+            $name = $data[1];
+            $price = "$".$data[2];
+            $percent = $data[3];
+            $results .= $ticker . "_" . $name . "_" . $price . "_" . $percent . "\n";
+        }
+      echo $results;
+    }	else{
 			echo "Error uploading file";
 		}
 	}
