d698914fbf0e29f333af72ece7e17d7f4d49f6d8
Zhongyang Gao <billgao0807@gmail.com>
2016-03-02 04:14:28 -0800

    fixed

--- js/load-watchlist.js
+++ js/load-watchlist.js
@@ -63,7 +63,7 @@ function addToWatchlist(new_stock_list){
 				tableRow.appendChild(tableData);
 				//assign action
 				var clickattr = document.createAttribute("onclick");
-				clickattr.value = "updateGraph("+stockData[0]+");";
+				clickattr.value = "updateGraph(\""+stockData[0]+"\");";
 				tableRow.setAttributeNode(clickattr);
 
 			}
