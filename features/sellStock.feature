a1995d013023308e61a5b191a0369370c8e93631
Rena <renachen@usc.edu>
2016-03-20 23:28:16 -0700

    finish buy sell test

--- features/sellStock.feature
+++ features/sellStock.feature
@@ -1,6 +1,6 @@
-Feature: Buy stock
+Feature: Sell stock
 
-	Scenario: Stock successfully bought when stock market open
+	Scenario: Stock successfully sold when market open
 		Given user is logged in on the dashboard:
 			|username|renachen@usc.edu|
 			|password|rc|
@@ -8,5 +8,7 @@ Feature: Buy stock
 			|Ticker|SEED|
 			|Quantity|1|
 		And the time in EST is between 9am and 4pm
-		And the user hits buy
+		And the user hits sell
 		Then balance and stock quantity should update
+
+		Then close window
--- features/step_definitions/buy_sell_steps.rb
+++ features/step_definitions/buy_sell_steps.rb
@@ -4,13 +4,15 @@ driver = Selenium::WebDriver.for :firefox
 @marketClosed = false
 
 Given(/^user is logged in on the dashboard:$/) do |table|
-  driver.navigate.to("http://localhost")
-  @userCredentials = table.rows_hash
-  driver.find_element(:id, "login-username").send_keys(@userCredentials['username'])
-  driver.find_element(:id, "login-password").send_keys(@userCredentials['password'])
-  driver.find_element(:id, "login-submit").click
-  currentURL = driver.current_url
-  expect(currentURL).to eq("http://localhost/mainpage.php")
+	if !driver.title.downcase.start_with? "portfolio"
+  		driver.navigate.to("http://localhost")
+ 		@userCredentials = table.rows_hash
+  		driver.find_element(:id, "login-username").send_keys(@userCredentials['username'])
+  		driver.find_element(:id, "login-password").send_keys(@userCredentials['password'])
+  		driver.find_element(:id, "login-submit").click
+  	end
+  		currentURL = driver.current_url
+  		expect(currentURL).to eq("http://localhost/mainpage.php")
 
   #initial balance
 balanceElement = driver.find_element(:id, "balance")
@@ -19,9 +21,12 @@ end
 
 When(/^the following stock information is input:$/) do |table|
   @buyInfo = table.rows_hash
+  #clear
+  driver.find_element(:id, "tickerInput").clear
+  driver.find_element(:id, "qty").clear
+  #input values
   driver.find_element(:id, "tickerInput").send_keys(@buyInfo['Ticker'])
   driver.find_element(:id, "qty").send_keys(@buyInfo['Quantity'])
-  driver.find_element(:id, "buy-button").click
 end
 
 When(/^the time in EST is between (\d+)am and (\d+)pm$/) do |startTime, endTime|
@@ -29,23 +34,36 @@ When(/^the time in EST is between (\d+)am and (\d+)pm$/) do |startTime, endTime|
 	time = driver.execute_script("return arguments[0].innerHTML;", element)
 	timeArray = time.split(/[\s:]/)
 
-	#wait longer for button to load
-	driver.manage.timeouts.implicit_wait = 3
-	puts timeArray[0]
-	puts timeArray[3]
+	puts "Time: "+timeArray[0]+timeArray[3]
 
 	if (timeArray[0].to_i >= startTime.to_i && timeArray[0].to_i != 12 && timeArray[3] == "AM") || (timeArray[0].to_i <= endTime.to_i && timeArray[3] == "PM") || (timeArray[0].to_i == 12 && timeArray[3] == "PM")
 		puts "Market open"
+		
+	else
+		puts "Market closed"
+		@marketClosed = true
+
+	end
+end
+
+When(/^the user hits buy$/) do
+	if !@marketClosed
+		driver.find_element(:id, "buy-button").click
 		driver.find_element(:id, "confBtn").click
 		#wait longer for button to appear
 		driver.manage.timeouts.implicit_wait = 10
 		driver.find_element(:id, "clsBtn").click
-	else
-		puts "Market closed"
-		@marketClosed = true
-		
-		driver.find_element(:id, 'clsBtn').click
+	end
 
+end
+
+When(/^the user hits sell$/) do
+	if !@marketClosed
+		driver.find_element(:id, "sell-button").click
+		driver.find_element(:id, "confBtn").click
+		#wait longer for button to appear
+		driver.managetimeouts.implicit_wait = 10
+		driver.find_element(:id, "clsBtn").click
 	end
 end
 
@@ -57,6 +75,8 @@ Then(/^balance and stock quantity should update$/) do
 		@newBalance = driver.execute_script("return arguments[0].innerHTML;", element)
 		expect(@newBalance).not_to eq(@initialBalance)
 	end
+end
 
+Then(/^close window/) do
 	driver.close
 end
